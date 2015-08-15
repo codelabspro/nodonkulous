5fabf81a08b440090e448cbab87628142366f8e5
Code SDK <codesdktutorials@gmail.com>
2015-08-15 14:44:18 -0400

    Added https

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+// Parsing using node.js
+var https = require("https");
+var username = "johndoe";
+
+function printMessage(username, badgeCount, points) {
+    var message = username + " has " + badgeCount + " total badges and " + points + " points in Javascript";
+    console.log(message);
+}
+
+printMessage("John", 1000, 2000000);
+//
+
+var request = https.get("https://sampletestdata.herokuapp.com/js/" + username + ".json", function(response) {
+    console.log(response.statusCode);
+});
